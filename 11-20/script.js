9c1c3105a1525beebc135455ebf6d6a7496d833b
Reniuha <naglisr45@gmail.com>
2025-01-08 16:20:08 +0200

    done

--- 11-20/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('eyeSaver').addEventListener('click', function() {
-    const body = document.body;
-    if (body.style.backgroundColor === 'rgb(59, 59, 59)') { 
-        body.style.backgroundColor = 'white'; 
-    } else {
-        body.style.backgroundColor = '#3b3b3b'; 
-    }
-});
